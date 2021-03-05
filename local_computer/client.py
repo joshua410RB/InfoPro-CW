08ac68041dc6e21796f7faf62fbc281ccaf64926
sytan98 <tan.siyu@dhs.sg>
2021-03-06 03:23:36 +0800

    trying out game feature

--- local_computer/client.py
+++ local_computer/client.py
@@ -4,15 +4,129 @@ from random import randint
 import time
 import logging
 import subprocess
+import pygame
+import random
 
 logging.basicConfig(level=logging.DEBUG,
                     format='[%(levelname)s] (%(threadName)-10s) %(message)s',
                     )
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+# HOST = '13.212.4.31'  # The server's hostname or IP address
+HOST = '127.0.0.1'
+PORT = 8000        # The port used by the server
 
 test_data = queue.Queue()
+movement_data = [0]
+
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+
+car_width = 73
+
+display_width = 800
+display_height = 600
+
+def things(gameDisplay, thingx, thingy, thingw, thingh, color):
+    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
+
+def car(gameDisplay, carImg, x,y):
+    gameDisplay.blit(carImg,(x,y))
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def message_display(gameDisplay, text, clock, carImg):
+    largeText = pygame.font.Font('freesansbold.ttf',115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+
+    time.sleep(2)
+
+    game_loop(gameDisplay, clock, carImg)
+    
+def crash(gameDisplay,clock, carImg):
+    message_display(gameDisplay, 'You Crashed', clock, carImg)
+    
+def game_loop(gameDisplay, clock, carImg):
+    x = (display_width * 0.45)
+    y = (display_height * 0.8)
+
+    x_change = 0
+
+    thing_startx = random.randrange(0, display_width)
+    thing_starty = -600
+    thing_speed = 7
+    thing_width = 100
+    thing_height = 100
+
+    gameExit = False
+
+    while not gameExit:
+
+        # for event in pygame.event.get():
+        #     if event.type == pygame.QUIT:
+        #         pygame.quit()
+        #         quit()
+
+        #     if event.type == pygame.KEYDOWN:
+        #         if event.key == pygame.K_LEFT:
+        #             x_change = -5
+        #         if event.key == pygame.K_RIGHT:
+        #             x_change = 5
+
+        #     if event.type == pygame.KEYUP:
+        #         if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+        #             x_change = 0
+
+        # x += x_change
+        x = movement_data[-1] 
+        logging.debug(x)
+        gameDisplay.fill(white)
+
+        # things(thingx, thingy, thingw, thingh, color)
+        things(gameDisplay, thing_startx, thing_starty, thing_width, thing_height, black)
+        thing_starty += thing_speed
+        car(gameDisplay, carImg,x,y)
+
+        if x > display_width - car_width or x < 0:
+            crash(gameDisplay, clock, carImg)
+
+        if thing_starty > display_height:
+            thing_starty = 0 - thing_height
+            thing_startx = random.randrange(0,display_width)
+
+        ####
+        if y < thing_starty+thing_height:
+            print('y crossover')
+
+            if x > thing_startx and x < thing_startx + thing_width or x+car_width > thing_startx and x + car_width < thing_startx+thing_width:
+                print('x crossover')
+                crash(gameDisplay, clock, carImg)
+        ####
+        
+        pygame.display.update()
+        clock.tick(60)
+
+def game_start():
+    pygame.init()
+
+    gameDisplay = pygame.display.set_mode((display_width,display_height))
+    pygame.display.set_caption('A bit Racey')
+    clock = pygame.time.Clock()
+
+    carImg = pygame.image.load('racecar.png')
+
+
+    game_loop(gameDisplay, clock, carImg)
+    pygame.quit()
+    quit()
+
+
 
 def generate_random():
     logging.debug("Starting Generation")
@@ -48,7 +162,11 @@ def receive_val(cmd):
             
             if (len(tmp[-1]) == 8):
                 out = twos_comp(int(tmp[-1],16), 32)
-                test_data.put(-1*int(out))
+                absolute_out = -1 * int(out)
+                test_data.put(absolute_out)
+                move = absolute_out / 300 * 800
+                # print(move)
+                movement_data.append(move)
                 # print(tmp[-1])
                 # print(test_data)
         except:
@@ -67,7 +185,7 @@ def start_client():
         while True:
             try:
                 item = test_data.get()
-                logging.debug(item)
+                # logging.debug(item)
                 sent = s.send(item.to_bytes(4,"big"))
             except IndexError:
                 logging.debug("Waiting")
@@ -89,7 +207,7 @@ def main():
     y = threading.Thread(target=start_client)
     x.start()
     y.start()
-
+    game_start()
     x.join()
     y.join()
 
