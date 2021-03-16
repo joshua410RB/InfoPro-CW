d6a8f6ecabb2b81d1a93cef1857018bbff05c458
joshua410RB <joshua.lim19@imperial.ac.uk>
2021-03-17 01:40:48 +0800

    added README.md to fpga_signal_path project

--- hardware/fpga_signal_path/programs/accel_test.c
+++ hardware/fpga_signal_path/programs/accel_test.c
@@ -14,27 +14,31 @@ int xbank;
 int ybank;
 int zbank;
 
-void readValues()	{
+//=========== Functions for interrupt handler ==========================
+void readValues()	{					// ISR to store new values into variables
 	x_data = IORD(ACCEL_XREAD_BASE,0);
 	y_data = IORD(ACCEL_YREAD_BASE,0);
 	z_data = IORD(ACCEL_ZREAD_BASE,0);
-	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(ACCEL_DATA_INTERRUPT_BASE, 0);
+	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(ACCEL_DATA_INTERRUPT_BASE, 0);	// clear interrupt edge capture register
 }
 
 void IRQInit(void *isr)	{
-	alt_irq_register(ACCEL_DATA_INTERRUPT_IRQ, 0, isr);
+	alt_irq_register(ACCEL_DATA_INTERRUPT_IRQ, 0, isr);	// register interrupt handler and ISR for fetching new values
 }
+//======================================================================
 
 int main()
 {
   printf("Hello from Nios II!\n");
 
+  //========== Important ==========================
   IRQInit(readValues);												// start interrupt handler for accelerometer values
   IOWR_ALTERA_AVALON_PIO_IRQ_MASK(ACCEL_DATA_INTERRUPT_BASE, 0xFF);
+  //================================================
 
   while(1)	{
-	  IOWR(PRINT0_BASE,0, x_data);
-	  IOWR(PRINT1_BASE,0, y_data);
+	  IOWR(PRINT0_BASE,0, x_data);	// print x-axis data to HEX 1-0
+	  IOWR(PRINT1_BASE,0, y_data);	// print y-axis data to HEX 5-4
 
 	  sw = IORD(SWITCH_BASE,0);
 	  xbank = sw&3;							// SW1-0 selects x-axis coefficient bank
