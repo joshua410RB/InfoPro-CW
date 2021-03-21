9dddc9c3a177152a213b0e674e4d2e064567f20c
jy3119 <jy3119@ic.ac.uk>
2021-03-21 15:52:03 +0000

    +bomb, score

--- local_computer/game_2.py
+++ local_computer/game_2.py
@@ -22,9 +22,11 @@ class Player(pygame.sprite.Sprite):
             return True
         else:
             return False
+
+    #JEONGIN
     def item_collect(self, item_group):
 	item_hit = pygame.sprite.spritecollide(self, item_group, True)
-	for item_hit:
+	if item_hit:
 	    self.bombnumber +=1
 
 class Obstacle(pygame.sprite.Sprite):
@@ -44,11 +46,11 @@ class Item(pygame.sprite.Sprite):
         self.rect.center = [pos_x, pos_y]
     def update(self, pos_x, pos_y):
         self.rect.center = [pos_x, pos_y]
-	#if collected:
-	#    self.kill()
-
-#    def collected(self)
-#	collected = pygame.sprite.spritecollide(self, player_group, True) 
+	if collected:
+	    self.kill()
+    #JEONGIN
+    def collected(self):
+	collected = pygame.sprite.spritecollide(self, player_group, True) 
 
 class Game():
     def __init__(self, x_data, y_data, display_width = 800, display_height = 600):
@@ -69,7 +71,7 @@ class Game():
         self.clock = pygame.time.Clock()
         # Set game objects
         self.bombImg = pygame.image.load('img/bomb.png')
-	self.bombnumber = 0
+	self.bombnumber = 0	#JEONGIN
         self.x_data = x_data
         self.y_data = y_data
         self.gameStart = False
@@ -93,6 +95,7 @@ class Game():
         time.sleep(2)
         # self.game_loop()    
 
+    #JEONGIN
     def score_display(self, text) :
     	font = pygame.font.Font('freesansbold.ttf', 80)
     	TextSurf, TextRect = self.text_objects(text, font)
@@ -214,13 +217,15 @@ class Game():
             obstacle_group.draw(self.screen)
             obstacle_group.update(obstacle_startx,obstacle_starty)
 
+	    #JEONGIN
+
             # item_group.draw(self.screen)
 	    item_group.draw(self.screen)
 	    item_group.update(item_startx, item_starty)
 
 	    #show score
 	    score_display(self, bombnumber)
-
+	    ######
 
             self.screen.blit(currspeed_text, currspeed_rect)
             self.screen.blit(time_text, time_rect)
